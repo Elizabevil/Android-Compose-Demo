22a50f280b392aa5e51688a3f1b8f658bfa687cf
Elizabevil <2197377595@qq.com>
2022-04-19 03:39:44 -0400

    init

--- app/src/main/java/com/eliza/askc/MainActivity.kt
+++ app/src/main/java/com/eliza/askc/MainActivity.kt
@@ -1,42 +1,101 @@
 package com.eliza.askc
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.eliza.library.model.User
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView()
-        setContent {
 
+
+        setContent {
+            MessageCard(name = "Sa", user = User("As", 12))
         }
     }
 
     @Composable
     fun MessageCard(name: String, user: User) {
-        Text(
-            text = "MessageCard:$name",
-            color = Color.Cyan
-        )
-        Text(text = user.toString())
+        Row(Modifier.padding(all = 8.dp)) {
+            Column() {
+                Text(
+                    text = "MessageCard:$name",
+                    color = Color.Cyan
+                )
+
+                Text(text = user.toString())
+            }
+            Image(
+                painter = painterResource(id = R.drawable.img),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+
+            )
+        }
 
     }
+
+    @Composable
+    fun ToOtherPage() {
+        val navController = rememberNavController()
+        val backstackEntry = navController.currentBackStackEntryAsState()
+        //获取当前的路由状态
+        val route = backstackEntry.value?.destination?.route
+//        3.创建并关联NavHost
+
+        NavHost(navController = navController, startDestination ="onePage" ){
+            composable("onePage") { FirstPage(navController = navController) }
+        }
+    }
+
+    /**
+     *FirstPage可组合项
+     */
+    @Composable
+    fun FirstPage(navController: NavController) {
+        Column {
+            Text(text = "FirstPage页面")
+            Button(onClick = {
+                // 导航到SecondPage可组合项
+                navController.navigate("second_page")
+            }) {
+                Text(text = "去SecondPage")
+            }
+        }
+    }
+
 
     /**
      * 仅作为 结果预览
      */
-    @Preview
+//    @Preview
     @Composable
     fun PreviewMessageCard() {
         MessageCard("as", User("As", 12))
---
+++ app/src/main/java/com/eliza/askc/layout/ListActivity.kt
@@ -0,0 +1,86 @@
+package com.eliza.askc.layout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.eliza.library.model.User
+
+
+/*-*- coding:utf-8 -*-
+ * @Author  : debi
+ * @Time    : 4/19/22
+ * @Software: Android Studio
+ */
+class ListActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView()
+        setContent {
+
+            ListUser(userList = ListData.userList)
+        }
+    }
+
+    @Composable
+    fun ListUser(userList: List<User>) {
+        LazyColumn {
+            items(userList) { user ->
+                MessageCard(user)
+            }
+        }
+    }
+
+    @Composable
+    fun MessageCard(user: User) {
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+        Spacer(modifier = Modifier.width(10.dp))
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = user.toString(),
+                modifier = Modifier.padding(4.dp),
+                color = Color.LightGray,
+                style = MaterialTheme.typography.body2,//字体
+                maxLines = if (isExpanded) Int.MAX_VALUE else 1,//只显示一
+            )
+        }
+
+
+    }
+
+//    @Preview
+    @Composable
+    fun Masin() {
+        ListUser(userList = ListData.userList)
+
+    }
+}
+
+
+object ListData {
+    val userList = listOf<User>(
+        User("1", 1),
+        User("2", 2),
+        User("3", 3),
+        User("4", 4),
+        User("5", 5),
+        User("6", 6),
+        User("7", 7),
+        User("8", 8),
+
+        )
+}
