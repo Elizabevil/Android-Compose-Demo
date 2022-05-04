16e43aad43d22a9902c568d6abaa8611391e128b
Elizabevil <2197377595@qq.com>
2022-05-04 03:42:55 -0400

    init

--- app/src/main/java/com/eliza/askc/layout/ListActivity.kt
+++ app/src/main/java/com/eliza/askc/layout/ListActivity.kt
@@ -27,7 +27,6 @@ class ListActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 //        setContentView()
         setContent {
-
             ListUser(userList = ListData.userList)
         }
     }
