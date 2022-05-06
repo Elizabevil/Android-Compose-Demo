6fdffcdc2b3370c828f35757e8532d5d54f63335
Elizabevil <2197377595@qq.com>
2022-05-06 05:10:41 -0400

    layouts

--- basic/src/main/java/com/eliza/comps/basic/layouts/FirstBaselineToTop.kt
+++ basic/src/main/java/com/eliza/comps/basic/layouts/FirstBaselineToTop.kt
@@ -12,19 +12,32 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.eliza.comps.basic.ui.theme.AskcTheme
 
+/*
+* 自定义修饰符
+* */
 fun Modifier.firstBaselineToTop(
     firstBaselineToTop: Dp
 ) = this.then(
     layout { measurable, constraints ->
+        //测量元素
         val placeable = measurable.measure(constraints)
 
         // Check the composable has a first baseline
+//        检查可组合物是否有第一个基线
         check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+
+        //测量之后，获取元素的基线值
         val firstBaseline = placeable[FirstBaseline]
 
         // Height of the composable with padding - first baseline
+        //元素新的Y坐标=新基线值-旧基线值
+        /* roundToPx： 通过四舍五入将 Dp 转换为 Int*/
         val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
         val height = placeable.height + placeableY
+
+        /*
+        * 最后要返回这一项，设置元素的宽高
+        * */
         layout(placeable.width, height) {
             // Where the composable gets placed
             placeable.placeRelative(0, placeableY)
@@ -36,7 +49,7 @@ fun Modifier.firstBaselineToTop(
 @Composable
 fun TextWithPaddingToBaselinePreview() {
     AskcTheme {
-        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+        Text("Hi there!", Modifier.firstBaselineToTop(30.dp))
     }
 }
 
--- basic/src/main/java/com/eliza/comps/basic/layouts/LayoutsCodelab.kt
+++ basic/src/main/java/com/eliza/comps/basic/layouts/LayoutsCodelab.kt
@@ -32,9 +32,10 @@ import com.eliza.comps.basic.ui.theme.AskcTheme
 import kotlin.math.max
 
 val topics = listOf(
-    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
-    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
-    "Religion", "Social sciences", "Technology", "TV", "Writing"
+    "Arts & Crafts 1", "Beauty 2", "Books 3", "Business 4", "Comics 5", "Culinary 6",
+    "Design 7", "Fashion 8", "Film 8", "History 9", "Maths 10", "Music 11", "People 12",
+    "Philosophy 13",
+    "Religion 14", "Social sciences 15", "Technology 16", "TV 17", "Writing 18"
 )
 
 @Composable
@@ -73,6 +74,14 @@ fun BodyContent(modifier: Modifier = Modifier) {
         })
 }
 
+/**
+ * Staggered grid
+ * 先行后列
+ * @param modifier
+ * @param rows 默认是3
+ * @param content
+ * @receiver
+ */
 @Composable
 fun StaggeredGrid(
     modifier: Modifier = Modifier,
@@ -85,29 +94,56 @@ fun StaggeredGrid(
     ) { measurables, constraints ->
 
         // Keep track of the width of each row
+        //用于保存每行的宽度值
         val rowWidths = IntArray(rows) { 0 }
 
         // Keep track of the max height of each row
+        //用于保存每行的高度值
         val rowHeights = IntArray(rows) { 0 }
 
         // Don't constrain child views further, measure them with given constraints
         // List of measured children
-//        不要进一步限制子视图，用给定的约束测量它们测量的子视图列表
+//       获取表格的宽高
         val placeables = measurables.mapIndexed { index, measurable ->
             // Measure each child
+            // 测量每一个元素
             val placeable = measurable.measure(constraints)
 
             // Track the width and max height of each row
+            //  跟踪每行的宽度和最大高度
+            // 通过取余 运算 获取下标
+            /*
+            row % 3
+            * 0 3 6 9
+            * 1
+            * 2
+            * */
             val row = index % rows
+
+            // -行的宽度等于这一 行所有元素宽度之和
+
             rowWidths[row] += placeable.width
+            // -行的 高度等于这一行所有元素 最高的元素
             rowHeights[row] = max(rowHeights[row], placeable.height)
 
             placeable
         }
 
+
+/*
+* rangeTo    创建从该值到指定的其他值的范围。
+* coerceIn ：确保此值位于指定范围内。
+    * 返回： 如果该值在范围内，则返回该值；
+    * 如果该值小于 range.start，则返回 range.start；
+    * 如果该值大于 range.endInclusive，则返回 range.endInclusive。
+* */
         // Grid's width is the widest row
-        val width = rowWidths.maxOrNull()
-            ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+        //若为空 则最小宽度值，否则给一个 范围值
+        val width =
+            rowWidths.maxOrNull()
+
+                ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth))
+                ?: constraints.minWidth
 
         // Grid's height is the sum of the tallest element of each row
         // coerced to the height constraints
@@ -117,6 +153,8 @@ fun StaggeredGrid(
         // Y of each row, based on the height accumulation of previous rows
         val rowY = IntArray(rows) { 0 }
         for (i in 1 until rows) {
+//            每一行元素的Y坐标 等于 它前一个元素的 y 坐标加其高度
+//            rowY[0] = rowY[0] + rowHeights[0]
             rowY[i] = rowY[i - 1] + rowHeights[i - 1]
         }
 
@@ -137,12 +175,19 @@ fun StaggeredGrid(
     }
 }
 
+/**
+ * Chip
+ * 单个卡片元素
+ * @param modifier
+ * @param text
+ */
 @Composable
 fun Chip(modifier: Modifier = Modifier, text: String) {
+    //一个卡片，圆角，里面包含一个Row，第-列是Box，第二列是文本
     Card(
         modifier = modifier,
-        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
-        shape = RoundedCornerShape(8.dp)
+        border = BorderStroke(color = Color.Blue, width = Dp.Hairline),//边框
+        shape = RoundedCornerShape(8.dp)//圆形边框的弧度
     ) {
         Row(
             modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
@@ -153,7 +198,7 @@ fun Chip(modifier: Modifier = Modifier, text: String) {
                     .size(16.dp, 16.dp)
                     .background(color = MaterialTheme.colors.secondary)
             )
-            Spacer(Modifier.width(4.dp))
+            Spacer(Modifier.width(10.dp))//间隙
             Text(text = text)
         }
     }
