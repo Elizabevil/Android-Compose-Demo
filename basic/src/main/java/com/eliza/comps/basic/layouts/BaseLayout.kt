814cea22774ab6a6f4885541ffc4aab95e88585b
Elizabevil <2197377595@qq.com>
2022-05-06 12:18:56 -0400

    baseLayout

--- basic/src/main/java/com/eliza/comps/basic/layouts/BaseLayout.kt
+++ basic/src/main/java/com/eliza/comps/basic/layouts/BaseLayout.kt
@@ -19,3 +19,9 @@ fun PhotographerCard() {
         }
     }
 }
+/*
+* CompositionLocal 类位于 androidx.compose.runtime 包下，总的来说是用于在 composition 树中共享变量的值。
+* 如果需要将顶层的 Composable 函数中的某个变量传递到最底层的 Composable 函数，
+    * 通常最简单有效的方法就是：1）定义一个全局变量，通过全局变量传值；
+    * 2）中间层的 Composable 函数添加一个形参，层层传递。
+    但是这两种方式都不太优雅，尤其是嵌套过深，或者数据比较敏感，不想暴露给中间层的函数时，这种情况下，就可以使用 CompositionLocal 来隐式的将数据传递给所需的 composition 树节点。*/
