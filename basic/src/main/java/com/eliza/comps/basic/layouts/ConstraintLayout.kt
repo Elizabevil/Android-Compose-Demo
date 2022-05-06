67b02405eeee99c5e5875cf132f9d2104bab2787
Elizabevil <2197377595@qq.com>
2022-05-06 07:45:52 -0400

    basic layouts

--- basic/src/main/java/com/eliza/comps/basic/layouts/ConstraintLayout.kt
+++ basic/src/main/java/com/eliza/comps/basic/layouts/ConstraintLayout.kt
@@ -14,15 +14,32 @@ import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
 import com.eliza.comps.basic.ui.theme.AskcTheme
 
+/*
+*
+        引用是使用createRefs()或createRefFor()创建的，ConstraintLayout 中的每个可组合项都需要有与之关联的引用。
+        约束条件是使用constrainAs()修饰符提供的，该修饰符将引用作为参数，可让您在主体lambda中指定其约束条件。
+        约束条件是使用linkTo() 或其他有用的方法指定的。
+        parent是一个现有的引用，可用于指定对ConstraintLayout可组合项本身的约束条件。
+* */
+/**
+ * Constraint layout content
+ * 最简单相对布局
+ */
 @Composable
 fun ConstraintLayoutContent() {
     ConstraintLayout {
+
+//        引用是使用createRefs()或createRefFor()创建的，ConstraintLayout 中的每个可组合项都需要有与之关联的引用。
+
         val (button, text) = createRefs()
 
         Button(
             onClick = { /* Do something */ },
+            //   约束条件是使用constrainAs()修饰符提供的，该修饰符将引用作为 第一个  参数
+            //在lambda表达式中指定约束规则
             modifier = Modifier.constrainAs(button) {
-                top.linkTo(parent.top, margin = 16.dp)
+//                约束条件是使用linkTo() 或其他有用的方法指定的
+                top.linkTo(parent.top, margin = 1.dp)
             }
         ) {
             Text("Button")
@@ -30,6 +47,7 @@ fun ConstraintLayoutContent() {
 
         Text("Text", Modifier.constrainAs(text) {
             top.linkTo(button.bottom, margin = 16.dp)
+            centerHorizontallyTo(parent)
         })
     }
 }
@@ -77,18 +95,26 @@ fun ConstraintLayoutContentExample2() {
 
         Button(
             onClick = { /* Do something */ },
+            /*
+            * button1 在父布局的上方，margin= *.dp
+            * */
             modifier = Modifier.constrainAs(button1) {
-                top.linkTo(parent.top, margin = 16.dp)
+
+                top.linkTo(parent.top, margin = 1.dp)
             }
         ) {
             Text("Button 1")
         }
-
+        /*
+            button1 的下方
+            * */
         Text("Text", Modifier.constrainAs(text) {
             top.linkTo(button1.bottom, margin = 16.dp)
             centerAround(button1.end)
         })
 
+
+        //将button1 和text组合起来，建立一个屏障 (barrier)
         val barrier = createEndBarrier(button1, text)
         Button(
             onClick = { /* Do something */ },
@@ -102,6 +128,9 @@ fun ConstraintLayoutContentExample2() {
     }
 }
 
+/**
+文字换行
+ * */
 @Composable
 fun LargeConstraintLayout() {
     ConstraintLayout {
@@ -112,6 +141,9 @@ fun LargeConstraintLayout() {
             "This is a very very very very very very very long text",
             Modifier.constrainAs(text) {
                 linkTo(start = guideline, end = parent.end)
+                /*
+                文字换行
+                * */
                 width = Dimension.preferredWrapContent
             }
         )
