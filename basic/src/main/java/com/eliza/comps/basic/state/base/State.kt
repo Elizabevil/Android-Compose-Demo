8e6743bb256bee0d03f7fe54898284d5fa02a4b8
Elizabevil <2197377595@qq.com>
2022-05-07 03:29:29 -0400

    State

--- basic/src/main/java/com/eliza/comps/basic/state/base/State.kt
+++ basic/src/main/java/com/eliza/comps/basic/state/base/State.kt
@@ -1,31 +1,25 @@
 package com.eliza.comps.basic.state.base
 
-import android.os.Bundle
-import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.eliza.comps.basic.ui.theme.AskcTheme
-import kotlinx.coroutines.delay
 
 /*
+https://developer.android.google.cn/jetpack/compose/state?hl=zh-cn
+
+
+mutableStateOf 会创建可观察的 MutableState<T>，后者是与 Compose 运行时集成的可观察类型。
+    interface MutableState<T> : State<T> {
+        override var value: T
+    }
+        value 如有任何更改，系统会安排重组读取 value 的所有可组合函数。对于 ExpandingCard，每当 expanded 发生变化时，都会导致重组 ExpandingCard。
 * Remember中文是记住，它的作用也类似，它会存储变量第一次赋值的结果，之后如果使用该变量时会去获取记录的值，之后如果值没有变化也就不会再次发生重组
-    val name = mutableStateOf("Bob")               // 1
-    val name by mutableStateOf("Bob")              // 2
-    val name by remember { mutableStateOf("Bob") } // 3
+        val mutableState = remember { mutableStateOf(default) }
+        var value by remember { mutableStateOf(default) }
+        val (value, setValue) = remember { mutableStateOf(default) }
 
 * */
 
