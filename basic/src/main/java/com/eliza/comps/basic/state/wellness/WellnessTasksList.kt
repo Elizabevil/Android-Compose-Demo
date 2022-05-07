e7dcab0d40d0b6176b520a48ca7ef95e7930c291
Elizabevil <2197377595@qq.com>
2022-05-07 11:07:10 -0400

    StateUp.kt

--- basic/src/main/java/com/eliza/comps/basic/state/wellness/WellnessTasksList.kt
+++ basic/src/main/java/com/eliza/comps/basic/state/wellness/WellnessTasksList.kt
@@ -1,10 +1,70 @@
 
 package com.eliza.comps.basic.state.wellness
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.eliza.comps.library.tools.InfoTools
+
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp),
+            text = taskName
+        )
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
+        )
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun WellnessTaskItemPreview() {
+    /*
+    *  taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier*/
+    WellnessTaskItem("taskName", true,
+        fun(state: Boolean) {
+            if (state) {
+                InfoTools.LogTools("WellnessTaskItem", "is $state")
+            }
+        },
+        fun() {}
+    )
+}
 
 @Composable
 fun WellnessTasksList(
